63037cad59b8aac89e02a694fe87cdd67a2743ab
jm.kagaya@gmail.com <jm.kagaya@gmail.com>
2019-05-26 17:55:25 +0900

    done
    done

---
+++ 02_Python_Syntax/main.py
@@ -0,0 +1,32 @@
+
+
+
+
+"""
+
+
+Python Indentations
+Where in other programming languages the indentation in code is for readability only, in Python the indentation is very important.
+
+Python uses indentation to indicate a block of code.
+
+"""
+if 5 > 2:
+   print("Hello World ")
+
+# Python will give you an error if you skip the indentation:
+if 5 > 2:
+    print(" True ")
+
+
+#Comments
+#Python has commenting capability for the purpose of in-code documentation.
+
+#Comments start with a #, and Python will render the rest of the line as a comment:
+
+#Multi Line Comments 
+"""
+This is a 
+multiline Comments
+
+"""
